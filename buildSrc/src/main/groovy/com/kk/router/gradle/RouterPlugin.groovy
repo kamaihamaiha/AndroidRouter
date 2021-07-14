b583ace840590b83d66ed904188842423548cdf5
kk <kamaihamaiha@gmail.com>
2021-07-14 23:13:29 +0800

    实战- 「路由」完成参数配置。

--- buildSrc/src/main/groovy/com/kk/router/gradle/RouterPlugin.groovy
+++ buildSrc/src/main/groovy/com/kk/router/gradle/RouterPlugin.groovy
@@ -8,5 +8,14 @@ class RouterPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         println("I'm from RouterPlugin, apply from ${project.name}")
+
+        // router 是随意起的名子
+        project.getExtensions().create("router", RouterExtension)
+
+        // gradle 配置阶段完成了
+        project.afterEvaluate {
+            RouterExtension extension = project["router"]
+            println("用户设置的 wikiStr 路径为：${extension.wikiStr}")
+        }
     }
 }
